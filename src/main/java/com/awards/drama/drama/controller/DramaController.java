b20e0a111983e4913a1f69284fa16fa7db754a25
Cherrue <th885172@gmail.com>
2022-01-20 23:04:58 +0900

    [refactor] 패키지 세분화 서비스, 컨트롤러 패키지 생성
    - 작업내용
    컨트롤러, 서비스 패키지 만들었음

---
+++ src/main/java/com/awards/drama/drama/controller/DramaController.java
@@ -0,0 +1,35 @@
+package com.awards.drama.drama.controller;
+
+import com.awards.drama.drama.services.DramaService;
+import com.awards.drama.drama.TestService;
+import com.awards.drama.entity.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("drama")
+public class DramaController {
+
+    private final TestService testService;
+    private final DramaService dramaService;
+
+    @Autowired
+    public DramaController(TestService testService, DramaService dramaService){
+        this.testService = testService;
+        this.dramaService = dramaService;
+    }
+
+    @GetMapping("apitest")
+    public Object testApi(){
+        return testService.getTestEntity();
+    }
+
+    @GetMapping("dbtest")
+    public List<Test> findAllTest(){
+        return testService.getTestAll();
+    }
+}
